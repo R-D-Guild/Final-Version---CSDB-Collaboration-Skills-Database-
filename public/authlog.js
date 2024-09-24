a9622766ba7cbf156680115c0a583aa3f4a1e4f8
IyinoluwAA <iyinoluwa150@gmail.com>
2024-09-24 07:41:53 -0700

    updated all

--- public/authlog.js
+++ public/authlog.js
@@ -114,7 +114,7 @@ loginForm.addEventListener('submit', async (event) => {
       // Success! Redirect to dashboard
       errorMessage.style.color = 'green';
       errorMessage.textContent = 'Login successful! Redirecting...';
-      window.location.href = '/dashardboard.html';
+      window.location.href = 'members/templates/members/members_list.html';
     }, 3000);
 
   } catch (error) {
